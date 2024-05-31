76e0868adfe40eeb744260cb651bb72e30d84faf
Marwa <marwa.niaaj@gmail.com>
2024-05-31 11:18:32 +0400

    Initial Commit

---
+++ LocationShare/LocationShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationShareApp.swift
+//  LocationShare
+//
+//  Created by Marwa Abou Niaaj on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocationShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
